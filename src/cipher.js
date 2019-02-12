615a96bccc08d1cb31c611d322a311f61f1a3a19
claudiaspandiar <claudia.spandiar@gmail.com>
2019-02-12 12:25:49 -0300

    subiendo cambios 12 de feb

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,41 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset1,messageEncode) => {
+    let solved = "";
+  
+  
+    for (let i=0; i < messageEncode.length; i++){
+     let asciinum = messageEncode.charCodeAt(i);
+     if (asciinum >= 65 && asciinum <= 90){
+     let ascii = ((asciinum - 65 + offset1) % 26 + 65);
+      solved += String.fromCharCode(ascii);
+     }else if (asciinum >= 97 && asciinum <= 122){
+     let ascii2 = ((asciinum - 97 + offset1) % 26 + 97);
+      solved += String.fromCharCode(ascii2);
+     }else {
+      solved += messageEncode;
+        }
+
+    }
+
+    return solved;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset2,messageDecode) => {
+    let solved1 = "";
+    
+  
+    for (let i=0; i < messageDecode.length; i++){
+     let asciinum1 = messageDecode.charCodeAt(i);
+     if (asciinum1 >= 65 && asciinum1 <= 90){
+     let ascii1 = ((asciinum1 + 65 - offset2) % 26 + 65);
+      solved1 += String.fromCharCode(ascii1);
+     }else if (asciinum1 >= 97 && asciinum1 <= 122){
+      let ascii3 = ((asciinum1 - 71 - offset2) % 26 + 97);
+       solved1 += String.fromCharCode(ascii3);
+      }else {
+      solved1 += messageDecode;
+        }
+
+    }
+    return solved1;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-/* Acá va tu código */
+//Diferentes pantallas
+document.getElementById("home1").addEventListener("click", () => {
+    document.getElementById("home").style.display="block";
+    document.getElementById("encrypt1").style.display="none";
+    document.getElementById("decode1").style.display="none";
+} )
+  
+document.getElementById("encrypt").addEventListener("click", () => { 
+    document.getElementById("home").style.display="none";
+    document.getElementById("encrypt1").style.display="block";
+    document.getElementById("decode1").style.display="none";
+  } )
+  
+document.getElementById("decode").addEventListener("click", () => {
+    document.getElementById("home").style.display="none";
+    document.getElementById("encrypt1").style.display="none";
+    document.getElementById("decode1").style.display="block";
+  } )
+
+  document.getElementById("home2").addEventListener("click", () => {
+    document.getElementById("home").style.display="block";
+    document.getElementById("encrypt1").style.display="none";
+    document.getElementById("decode1").style.display="none";
+} )
+  
+  //mensaje cifrado
+document.getElementById("encode").addEventListener("click", () => {
+    let messageEncode = document.getElementById("encryptxt").value;
+    let offset1 = parseInt(document.getElementById("offsetEncode").value);
+
+  document.getElementById("result1").innerHTML = cipher.encode(offset1,messageEncode);
+  })
+
+  //mensaje descifrado
+ document.getElementById("decode2").addEventListener("click", () => {
+    let messageDecode = document.getElementById("decodetxt").value;
+    let offset2 = parseInt(document.getElementById("offsetDecode").value);
+  
+  document.getElementById("result2").innerHTML = cipher.decode(offset2,messageDecode);
+  })
+  
